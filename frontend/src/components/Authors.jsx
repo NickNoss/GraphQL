1a96a5259323391a5d9f77b573135adbfe512a53
NickNoss <nnossenko@gmail.com>
2025-08-12 13:39:29 +0300

    Part 8.8, initial commit

---
+++ frontend/src/components/Authors.jsx
@@ -0,0 +1,53 @@
+import { useQuery, gql } from '@apollo/client'
+import PropTypes from 'prop-types'
+
+const ALL_AUTHORS = gql`
+  query {
+    allAuthors {
+      name
+      born
+      bookCount
+    }
+  }
+`
+
+const Authors = (props) => {
+  const { loading, error, data } = useQuery(ALL_AUTHORS)
+
+  if (!props.show) {
+    return null
+  }
+
+  if (loading) return <div>loading...</div>
+  if (error) return <div>Error: {error.message}</div>
+
+  const authors = data.allAuthors
+
+  return (
+    <div>
+      <h2>authors</h2>
+      <table>
+        <tbody>
+          <tr>
+            <th></th>
+            <th>born</th>
+            <th>books</th>
+          </tr>
+          {authors.map((a) => (
+            <tr key={a.name}>
+              <td>{a.name}</td>
+              <td>{a.born}</td>
+              <td>{a.bookCount}</td>
+            </tr>
+          ))}
+        </tbody>
+      </table>
+    </div>
+  )
+}
+
+Authors.propTypes = {
+  show: PropTypes.bool.isRequired,
+}
+
+export default Authors
