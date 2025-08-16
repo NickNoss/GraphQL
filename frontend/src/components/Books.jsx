bfe0c770ca06fb847d2dde6d526a6436526931e7
NickNoss <nnossenko@gmail.com>
2025-08-16 12:54:45 +0300

    Osa8.19 genresuodatus

--- frontend/src/components/Books.jsx
+++ frontend/src/components/Books.jsx
@@ -1,6 +1,7 @@
 // import apollo client and necessary hooks
 import { useQuery, gql } from '@apollo/client'
 import PropTypes from 'prop-types'
+import { useState } from 'react'
 
 // GraphQL query to fetch all books
 const ALL_BOOKS = gql`
@@ -20,6 +21,8 @@ const Books = (props) => {
   // use the useQuery hook to fetch books
   const { loading, error, data } = useQuery(ALL_BOOKS)
 
+  const [selectedGenre, setSelectedGenre] = useState(null)
+
   if (!props.show) {
     return null
   }
@@ -29,9 +32,20 @@ const Books = (props) => {
 
   const books = data.allBooks
 
+  const allGenres = [...new Set(books.flatMap(book => book.genres))]
+  const filteredBooks = selectedGenre
+    ? books.filter(book => book.genres.includes(selectedGenre))
+    : books
+
   return (
     <div>
       <h2>books</h2>
+
+      {selectedGenre && (
+        <p>
+          in genre <strong>{selectedGenre}</strong>
+        </p>
+      )}
 
       <table>
         <tbody>
@@ -40,7 +54,7 @@ const Books = (props) => {
             <th>author</th>
             <th>published</th>
           </tr>
-          {books.map((a) => (
+          {filteredBooks.map((a) => (
             <tr key={a.title}>
               <td>{a.title}</td>
               <td>{a.author.name}</td>
@@ -49,6 +63,15 @@ const Books = (props) => {
           ))}
         </tbody>
       </table>
+
+      <div style={{ marginTop: '1em' }}>
+        <button onClick={() => setSelectedGenre(null)}>all genres</button>
+        {allGenres.map((genre) => (
+          <button key={genre} onClick={() => setSelectedGenre(genre)}>
+            {genre}
+          </button>
+        ))}
+      </div>
     </div>
   )
 }
